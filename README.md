125e6cb3b69e477d98ea2468e95e07051cc31e45
Nadia HYLARY <nhylary12@gmail.com>
2022-10-05 12:07:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# world_feminists_network
+ A flutter app for the members(and aspiring members) of a feminists community
---
+++ android/app/src/main/kotlin/com/nh/startup_namer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nh.startup_namer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
